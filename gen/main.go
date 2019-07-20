c66cc27a84d722243ad313b999689b54548fd07e
NickLarsenNZ <nick@aptiv.co.nz>
2019-07-21 01:55:02 +1200

    Implementing the template renderer for #8 - Abstracted code generation so we can generate FSM code for more than one FSM library - Allow post processing commands in a pipeline - Resolves #5

---
+++ gen/main.go
@@ -0,0 +1,67 @@
+package gen
+
+import (
+	"github.com/joncalhoun/pipe"
+	"io"
+	"io/ioutil"
+	"log"
+	"os/exec"
+	"text/template"
+)
+
+type Renderer interface {
+	Render(buffer io.Writer) error
+	PipeTo(command string, arg ...string)
+}
+
+type renderer struct {
+	template        string
+	data            interface{}
+	commandPipeline []*exec.Cmd
+}
+
+func (r *renderer) PipeTo(command string, arg ...string) {
+	r.commandPipeline = append(r.commandPipeline, exec.Command(command, arg...))
+}
+
+func (r renderer) Render(buffer io.Writer) error {
+	t := template.Must(template.New("fsm").Parse(r.template))
+
+	if len(r.commandPipeline) == 0 {
+		// If there are no commands in the pipeline, simply render the template
+		return t.Execute(buffer, &r.data)
+	} else {
+		// Otherwise we need to render, then pipe the output through the commands in the list
+		rc, wc, _ := pipe.Commands(
+			r.commandPipeline...,
+		)
+		err := t.Execute(wc, &r.data)
+		if err != nil {
+			return err
+		}
+
+		err = wc.Close()
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(buffer, rc)
+		if err != nil {
+			return err
+		}
+
+		return nil
+	}
+}
+
+func NewRenderer(template io.Reader, data interface{}) Renderer {
+	buf, err := ioutil.ReadAll(template)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &renderer{
+		template: string(buf),
+		data:     data,
+	}
+}
---
+++ gen/main_test.go
@@ -0,0 +1,86 @@
+package gen_test
+
+import (
+	"bytes"
+	"github.com/nicklarsennz/dot-fsm/gen"
+	"strings"
+	"testing"
+)
+
+func TestTemplateRender(t *testing.T) {
+	template := `
+{{range .Transitions -}}
+{{.Name}}
+{{end -}}
+`
+	expected := `
+t1
+t2
+`
+	templateData := struct {
+		Transitions []struct {
+			Name string
+		}
+	}{
+		Transitions: []struct{ Name string }{
+			{Name: "t1"},
+			{Name: "t2"},
+		},
+	}
+
+	byteWriter := bytes.NewBuffer([]byte{})
+	stringReader := strings.NewReader(template)
+
+	renderer := gen.NewRenderer(stringReader, templateData)
+	err := renderer.Render(byteWriter)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := byteWriter.String()
+
+	if actual != expected {
+		t.Fatalf("rendering the template failed. Expected '%s', got '%s'", expected, actual)
+	}
+}
+
+func TestTemplateRenderWithCommands(t *testing.T) {
+	template := `
+
+
+ package gen
+
+   func {{.Thing}}()     {
+_ = 0
+ }
+
+
+`
+	expected := `package gen
+
+func blah() {
+	_ = 0
+}
+`
+	templateData := struct {
+		Thing string
+	}{
+		Thing: "blah",
+	}
+
+	byteWriter := bytes.NewBuffer([]byte{})
+	stringReader := strings.NewReader(template)
+
+	renderer := gen.NewRenderer(stringReader, templateData)
+	renderer.PipeTo("gofmt")
+	err := renderer.Render(byteWriter)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual := byteWriter.String()
+
+	if actual != expected {
+		t.Fatalf("rendering the template failed. Expected '%s', got '%s'", expected, actual)
+	}
+}
